98fe83154f1919514ab34e099de5dfa6e2d6ff05
martin <martin.alvarado2002@gmail.com>
2023-09-16 20:50:53 +0200

    [#31-280]:  Writing Tests

--- test/script.test.js
+++ test/script.test.js
@@ -1,15 +1,23 @@
 const gaagleSearch = require('./script');
 
 const dbMock = ['dog.com', 'cheesepuff.com', 'disney.com', 'dogpictures.com'];
-it('this is a test', () => {
-  gaagleSearch('test', dbMock);
-  // console.log(
-  //   `🔎 | Testing | gaagleSearch > test:`,
-  //   gaagleSearch('dog', dbMock)
-  // );
-});
 
-it('is serching gaagle', () => {
-  expect(gaagleSearch('test', dbMock)).toEqual([]);
-  expect(gaagleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com']);
+describe('Gaagle Search', () => {
+  it('this is a test', () => {
+    gaagleSearch('test', dbMock);
+  });
+
+  it('is serching gaagle', () => {
+    expect(gaagleSearch('test', dbMock)).toEqual([]);
+    expect(gaagleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com']);
+  });
+
+  it('will work with undefined and null', () => {
+    expect(gaagleSearch(undefined, dbMock)).toEqual([]);
+    expect(gaagleSearch(null, dbMock)).toEqual([]);
+  });
+
+  it('Does not return more than 3 matches', () => {
+    expect(gaagleSearch('.com', dbMock).length).toEqual(3);
+  });
 });
