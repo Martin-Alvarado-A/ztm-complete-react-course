9b0e542ee78e4d5e6aa9cd9071e7f7a74afbdaa3
martin <martin.alvarado2002@gmail.com>
2023-09-19 00:46:49 +0200

    [#31-306]: Navigation Tests - Part 2

--- crwn-clothing-rtl/src/routes/navigation/__tests__/navigation.component.test.js
+++ crwn-clothing-rtl/src/routes/navigation/__tests__/navigation.component.test.js
@@ -34,4 +34,47 @@ describe('Navigation Tests', () => {
     const signinLinkElement = screen.queryByText(/sign in/i);
     expect(signinLinkElement).toBeNull();
   });
+
+  test('It should render the cart Dropdown if button is pressed', () => {
+    renderWithProviders(<Navigation />, {
+      preloadedState: {},
+    });
+
+    expect.assertions(1);
+    const cartIconElement = screen.getByText(/0/i);
+    fireEvent.click(cartIconElement);
+
+    const emptycartElement = screen.getByText(/your cart is empty/i);
+    expect(emptycartElement).toBeInTheDocument();
+  });
+
+  test('It should not render the Cart Dropdown if isCartOpen is false', () => {
+    renderWithProviders(<Navigation />, {
+      preloadedState: {
+        cart: {
+          isCartOpen: false,
+          cartItems: [],
+        },
+      },
+    });
+
+    expect.assertions(1);
+    const dropdownTextElement = screen.queryByText(/your cart is empty/i);
+    expect(dropdownTextElement).toBeNull();
+  });
+
+  test('It should render the Cart Dropdown if isCartOpen is true', () => {
+    renderWithProviders(<Navigation />, {
+      preloadedState: {
+        cart: {
+          isCartOpen: true,
+          cartItems: [],
+        },
+      },
+    });
+
+    expect.assertions(1);
+    const dropdownTextElement = screen.getByText(/your cart is empty/i);
+    expect(dropdownTextElement).toBeInTheDocument();
+  });
 });
